83ab0957c3aa86a514561a833bc06a8355741708
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 22:51:40 +0800

    Create XP-EHH.sh

---
+++ Exploration on genomic evolution of GR population/XP-EHH.sh
@@ -0,0 +1,20 @@
+#XP-EHH
+for i in {1..19}
+do
+  vcftools --gzvcf autosome.vcf.gz --chr $i --recode --out ./data/CP-$i --keep CP.txt
+  vcftools --gzvcf autosome.vcf.gz --chr $i --recode --out ./data/GR-$i --keep JL.txt
+  plink --vcf ./data/CP-$i.recode.vcf --recode --out ./map/$i
+  awk '$3=$4*0.00000057' ./map/$i.map > ./map/$i-cM.map
+done
+
+
+for i in {1..19}
+do 
+    selscan --xpehh --vcf ./data/GR-$i.recode.vcf --vcf-ref ./data/CP-$i.recode.vcf --map ./map/$i-cM.map --threads 6 --out selscan-$i
+done
+
+
+for i in {1..19}
+do
+Rscript Sliding_window.R  -i ./selscan-$i.xpehh.out -w 40000 -s 20000 -o chr$i
+done
