21301f698db34bf5b247179d5b3aa1a211fdd83a
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 22:47:00 +0800

    Create SMC++.sh

---
+++ Genomic diversity, divergence time, and demography history of M. musculus/SMC++.sh
@@ -0,0 +1,16 @@
+#vcf to vcf.gz
+bgzip 5pop.recode.vcf
+tabix 5pop.recode.vcf.gz
+
+#vcf2smc
+for j in {1..19}
+do
+  sudo docker run --rm -v $PWD:/mnt terhorst/smcpp:latest vcf2smc --mask ./mask/chr${j}.mask.bed.gz ./5pop.recode.vcf.gz ./CAS1/CAS1-chr${j}.smc.gz $j CAS1:IN1,IN2,IN3,IN4,IN5,IN6,PA1,PA2,PA3,PA4
+done
+
+#estimate
+sudo docker run --rm -v $PWD:/mnt terhorst/smcpp:latest estimate --spline cubic --knots 15 --timepoints 100 10000000 --cores 20  -o ./CAS1/estimate/ 4.1e-9 ./CAS1/CAS1-*.smc.gz
+
+#plot
+sudo docker run --rm -v $PWD:/mnt terhorst/smcpp:latest plot ./total.pdf ./*/estimate/*.final.json -g 0.5 --ylim 0 10000000 --xlim 1 1000000 -c
+
