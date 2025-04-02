ce546242fc53202dac5fbd0c60f5a53c540aa7c2
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-03 02:30:18 +0800

    Update 4.SMC++.sh

--- Genomic diversity, divergence time, and demography history of M. musculus/4.SMC++.sh
+++ Genomic diversity, divergence time, and demography history of M. musculus/4.SMC++.sh
@@ -5,7 +5,7 @@ tabix 5pop.recode.vcf.gz
 #vcf2smc
 for j in {1..19}
 do
-  sudo docker run --rm -v $PWD:/mnt terhorst/smcpp:latest vcf2smc --mask ./mask/chr${j}.mask.bed.gz ./5pop.recode.vcf.gz ./CAS1/CAS1-chr${j}.smc.gz $j CAS1:IN1,IN2,IN3,IN4,IN5,IN6,PA1,PA2,PA3,PA4
+  sudo docker run --rm -v $PWD:/mnt terhorst/smcpp:latest vcf2smc --mask ./mask/chr${j}.mask.bed.gz ./5pop.recode.vcf.gz ./CAS1/CAS1-chr${j}.smc.gz $j CAS1:IN1,IN2,IN3,IN4,IN5,IN6,IN7,IN8,IN9,IN10
 done
 
 #estimate
