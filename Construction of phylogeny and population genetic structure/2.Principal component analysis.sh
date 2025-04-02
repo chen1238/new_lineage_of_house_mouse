7839f1ed01173dea0ad7d0b29a803d41d7cb2beb
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 23:56:28 +0800

    Update 2.Principal component analysis.sh

--- Construction of phylogeny and population genetic structure/2.Principal component analysis.sh
+++ Construction of phylogeny and population genetic structure/2.Principal component analysis.sh
@@ -1,5 +1,5 @@
-#vcftools --vcf ../Autosome.vcf --recode --mac 3 --max-missing 0.9 --keep sample.txt --out Autosome
+#vcftools --vcf ../Autosome.vcf --recode --mac 3 --max-missing 0.9 --keep sample.txt --out autosome
 
-python2 ./vcf2eigenstrat.py -v ./Autosome.recode.vcf -o autosome
+python2 ./vcf2eigenstrat.py -v ./autosome.recode.vcf -o autosome
 
 smartpca -p runningpca.conf.txt
