b58ca79a66d8444873c0baca98aeb142d0a6a4b8
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 23:55:15 +0800

    Update and rename Principal component analysis.sh to 2.Principal component analysis.sh

--- Construction of phylogeny and population genetic structure/2.Principal component analysis.sh
+++ Construction of phylogeny and population genetic structure/2.Principal component analysis.sh
@@ -1,4 +1,5 @@
 #vcftools --vcf ../Autosome.vcf --recode --mac 3 --max-missing 0.9 --keep sample.txt --out Autosome
+
 python2 ./vcf2eigenstrat.py -v ./Autosome.recode.vcf -o autosome
 
 smartpca -p runningpca.conf.txt
