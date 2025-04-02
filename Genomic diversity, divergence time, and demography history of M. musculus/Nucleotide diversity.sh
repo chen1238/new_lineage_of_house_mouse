f944802d3c5d62a2e562728194b87d167f390317
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 22:19:15 +0800

    Update and rename 1 to Nucleotide diversity.sh

---
+++ Genomic diversity, divergence time, and demography history of M. musculus/Nucleotide diversity.sh
@@ -0,0 +1,10 @@
+../angsd/angsd -bam GR.list -doSaf 1 -out GR -GL 2 -anc /media/rui/4t/Rgrcm38p6/grcm38.fa
+
+
+../angsd/misc/realSFS GR.saf.idx -fold 1 > GR.sfs
+
+
+../angsd/misc/realSFS saf2theta -sfs GR.sfs GR.saf.idx -fold 1 -outname GR
+
+
+../angsd/misc/thetaStat do_stat GR.thetas.idx -win 40000 -step 40000 -outnames GR.theta.windows
