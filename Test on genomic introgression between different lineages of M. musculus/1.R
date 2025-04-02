0d287ac5956d557d0b2c53d5c3f88d37aa8bb10f
chen1238 <65192478+chen1238@users.noreply.github.com>
2025-04-02 21:48:04 +0800

    Add files via upload

---
+++ Test on genomic introgression between different lineages of M. musculus/1.R
@@ -0,0 +1,17 @@
+rm(list=ls())
+library(admixtools)
+library(tidyverse)
+
+
+prefix='./data/autosome'
+
+
+DOM=read.table('DOM.txt')$V1
+BAC=read.table('BAC.txt')$V1
+Target=read.table('Target.txt')$V1
+SPR=c("SP1","SP4")
+
+
+a=f4('./data/autosome',c(BAC,Target),BAC,DOM,SPR,f4mode=FALSE)
+write.csv(a,file='BABA-ABBA-T-BAC-DOM.csv',row.names=F)
+
